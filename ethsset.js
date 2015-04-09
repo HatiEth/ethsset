8303a6f0e4c2afef511d8628cb2dd72bf38553ee
Hati Eth <hati.eth@gmail.com>
2015-04-09 18:13:21 +0200

    removing minimist again

--- ethsset.js
+++ ethsset.js
@@ -4,38 +4,20 @@ var fs = require('fs');
 var Util = require('util');
 var NinjaGen = require('ninja-build-gen');
 var Ninja = NinjaGen('1.5.3', 'ethsset_build');
-
-if(!fs.existsSync(ConfigFilePath))
-{
+if (!fs.existsSync(ConfigFilePath)) {
     console.log('Creating default ethsset.json');
     var Config = {
         rules: [],
         edges: []
     };
-
     fs.writeFileSync(ConfigFilePath, JSON.stringify(Config, null, 4));
 }
-
 var EthssetConfig = require(ConfigFilePath);
 var sys = require('sys');
 var exec = require('child_process').exec;
-
 var Rules = EthssetConfig.rules;
 var Edges = EthssetConfig.edges;
-
-interface rule {
-    rule: string;
-    cmd: string;
-    desc: string;
-}
-
-interface rules {
-    [index: number]: rule;
-}
-
-function printRules(config: {Rules: rules})
-{
+function printRules(config) {
     console.log(Rules);
 }
-
 printRules(Rules);
